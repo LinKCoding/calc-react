c7706be7de642d9c45ff36e98cd73838d84980d5
linkcoding <kenny.lin.91@gmail.com>
2020-01-12 15:31:56 -0500

    changed names of button list

--- src/components/calculatorComps/buttonList.js
+++ src/components/calculatorComps/buttonList.js
@@ -4,13 +4,13 @@ import Col from 'react-bootstrap/Col'
 
 const ButtonList = (props) => {
   const sortedButtons = []
-  for(let i = 0; i < props.chars.length; i += 4) {
+  for (let i = 0; i < props.chars.length; i += 4) {
     sortedButtons.push(props.chars.slice(i, i + 4))
   }
   return (
     <Col>
       {sortedButtons.map((row, idx) => {
-        return <ButtonRow key={`${row}-${idx}`} row={row} addToEquation={props.addToEquation}/>
+        return <ButtonRow key={`${row}-${idx}`} row={row} addToEquation={props.addToEquation} />
       })}
     </Col>
   )
